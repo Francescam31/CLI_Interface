554f4742b46a9e3279b17330a75610bd34930340
Francesca Marshall <francescam3105@gmail.com>
2023-07-25 12:39:49 +0100

    Tested Airport and Flight

--- CLI_Interface/src/test/java/AirportTest.java
+++ CLI_Interface/src/test/java/AirportTest.java
@@ -1,23 +1,46 @@
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AirportTest {
+    Flight flight;
+    Airport airport;
+    Passenger passenger;
+
 
     @BeforeEach
+    public void setUp(){
+         airport = new Airport("Heathrow");
+         flight = new Flight("EasyJet",1);
+         passenger = new Passenger("Zaiyad", "1234", 1);
+    }
 
 
     @Test
     void addNewFlight() {
+
+        airport.addNewFlight(flight);
+        assertThat(airport.getFlights().size()).isEqualTo(1);
     }
 
     @Test
     void cancelFlight() {
+         airport.addNewFlight(flight);
+         assertThat(airport.getFlights().size()).isEqualTo(1);
+         airport.cancelFlight(flight);
+         assertThat(airport.getFlights().size()).isEqualTo(0);
+
     }
 
     @Test
     void bookPassengerOnFlight() {
+        airport.bookPassengerOnFlight(passenger,flight);
+        assertThat(flight.getPassengers().size()).isEqualTo(1);
+        assertThat(airport.getFlights().size()).isEqualTo(1);
+
     }
 
     @Test
