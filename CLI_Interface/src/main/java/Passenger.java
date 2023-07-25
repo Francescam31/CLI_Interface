66b1c179508bcb1c4ae3d7d01902c79f7958a520
Francesca Marshall <francescam3105@gmail.com>
2023-07-25 11:06:59 +0100

    Completed Passenger class

--- CLI_Interface/src/main/java/Passenger.java
+++ CLI_Interface/src/main/java/Passenger.java
@@ -1,2 +1,36 @@
 public class Passenger {
+
+    private String name;
+    private String contactInfo;
+    private int uniqueId;
+
+    public Passenger(String name, String contactInfo, int uniqueId) {
+        this.name = name;
+        this.contactInfo = contactInfo;
+        this.uniqueId = uniqueId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return this.contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public int getUniqueId() {
+        return this.uniqueId;
+    }
+
+    public void setUniqueId(int uniqueId) {
+        this.uniqueId = uniqueId;
+    }
 }
